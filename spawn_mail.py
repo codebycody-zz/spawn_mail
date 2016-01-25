2c835b601234aa6b4ed4f29652b53156c5e914fb
codebycody <cody@archonweb.com>
2016-01-24 23:09:00 -0500

    finally a usable tool! TODO:add filter for male and female names

--- spawn_mail.py
+++ spawn_mail.py
@@ -1,37 +1,39 @@
-from itertools import islice
-from random    import choice, randint
-from string    import ascii_lowercase
+import sys, getopt
+
+domain = ''
+domain_file = ''
+
+def main(argv):
+
+	def printEmails(emails):
+		target = open(domain_file, 'w')
+
+		for email in emails:
+			target.write(email)
+			target.write('\n')
+
+	for arg in argv[0:]:
+		domain = '@' + arg
+		domain_file = 'email_lists/' + arg.replace('.', '') + '.txt'
+
+	with open("names/female.first.txt", "r") as ins:
+		emails = []
+		for line in ins:
+			email = line.rstrip('\n') + domain
+			emails.append(email)
+
+	printEmails(emails)
+
+	with open("names/male.first.txt", "r") as ins:
+		emails = []
+		for line in ins:
+			email = line.rstrip('\n') + domain
+			emails.append(email)
+
+	printEmails(emails)
 
 
-# top-level domains
-TLDS = ('com net org mil edu de biz de ch at ru de tv com'
-    'st br fr de nl dk ar jp eu it es com us ca pl').split()
 
-def gen_name(length):
-    """Generate a random name with the given number of characters."""
-    return ''.join(choice(ascii_lowercase) for _ in xrange(length))
-
-def address_generator():
-    """Generate fake e-mail addresses."""
-    while True:
-        user = gen_name(randint(3, 10))
-        host = gen_name(randint(4, 20))
-        yield '%s@%s.%s' % (user, host, choice(TLDS))
-
-def markup_address(address):
-    """Wrap an e-mail address in an XHTML "mailto:" anchor."""
-    return '<a href="mailto:%s">%s</a>' % ((address,) * 2)
-
-def fake_addresses(count=20, sep=', ', markup=False):
-    """Generate fake e-mail addresses.
-
-    If ``markup`` is true, turn the addresses into "mailto:" XHTML anchors.
-    """
-    addresses = islice(address_generator(), count)
-    if markup:
-        addresses = map(markup_address, addresses)
-    return sep.join(addresses)
 
 if __name__ == '__main__':
-    with open("email_list.txt", "a") as myfile:
-        myfile.write(fake_addresses(sep='\n'))
+	main(sys.argv[1:])
