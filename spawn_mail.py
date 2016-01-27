149772399fc1a564880ca30a71429c31a80bf2f0
codebycody <cody@archonweb.com>
2016-01-26 22:05:11 -0500

    got rid of the male vs. female naming and just put everyone under one file.

--- spawn_mail.py
+++ spawn_mail.py
@@ -16,24 +16,13 @@ def main(argv):
 		domain = '@' + arg
 		domain_file = 'email_lists/' + arg.replace('.', '') + '.txt'
 
-	with open("names/female.first.txt", "r") as ins:
+	with open("names/first_names.txt", "r") as ins:
 		emails = []
 		for line in ins:
 			email = line.rstrip('\n') + domain
 			emails.append(email)
 
 	printEmails(emails)
-
-	with open("names/male.first.txt", "r") as ins:
-		emails = []
-		for line in ins:
-			email = line.rstrip('\n') + domain
-			emails.append(email)
-
-	printEmails(emails)
-
-
-
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
